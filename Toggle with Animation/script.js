3981bf91de7680a3c7183c3eea7980bb6c659139
Om Veerwal <omveerwal9@gmail.com>
2025-07-09 13:08:41 +0530

    fixing issue in toggle-animation

--- Toggle with Animation/script.js
+++ Toggle with Animation/script.js
@@ -1,16 +1,19 @@
-const toggleBtn = document.getElementById("toggleBtn");
-const box = document.getElementById("box");
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.getElementById("toggleBtn");
+  const message = document.getElementById("message");
 
-let isVisible = false;
+  let isVisible = false;
 
-toggleBtn.addEventListener("click", () => {
-  if (!isVisible) {
-    box.classList.add("show");
-    box.classList.remove("hide");
-  } else {
-    box.classList.add("hide");
-    box.classList.remove("show");
-  }
+  toggleBtn.addEventListener("click", () => {
+    console.log("Button clicked");
+    if (!isVisible) {
+      message.style.display = "block";
+      toggleBtn.textContent = "Hide Message"
+    } else {
+      message.style.display="none";
+      toggleBtn.textContent="show Message";
+    }
 
-  isVisible = !isVisible;
+    isVisible = !isVisible;
+  });
 });
